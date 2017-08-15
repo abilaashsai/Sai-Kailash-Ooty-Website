2b029433fe13084df18881d6eb59053ae775de26
abilashr <abilaashsai@gmail.com>
2017-08-16 01:10:30 +0530

    Add para functionality to events

--- js/eventDetailsEdit.js
+++ js/eventDetailsEdit.js
@@ -79,44 +79,126 @@ function addDetailsIntoDropDown(year, month, date, message) {
     option.text = year + "-" + month + "-" + date + " " + message;
     eventAdded.add(option);
 };
-
+function addParaDetailsIntoDropDown(number) {
+    var paraDropDown = document.getElementById("para");
+    var option = document.createElement("option");
+    option.value = number;
+    option.text = number;
+    paraDropDown.add(option);
+}
 function checkInformation() {
 
-    removeOptions(document.getElementById("deleteEventImage"));
     if (getSelectedEvent() === "") {
         document.getElementById("eventType").value = "";
-        document.getElementById("writeup").value = "";
-        document.getElementById("uploadFileName").value = "";
     } else {
         firebase.database().ref('events/upcoming/' + getSelectedEvent()).once('value').then(function (snapshot) {
             document.getElementById("eventType").value = snapshot.val().type;
-            document.getElementById("writeup").value = snapshot.val().detail;
-            snapshot.forEach(function (image) {
-                if (image.key == "image") {
-                    image.forEach(function (imageName) {
-                            var imageTitle = document.getElementById("deleteEventImage");
-                            var option = document.createElement("option");
-                            option.value = imageName.key;
-                            option.text = imageName.key;
-                            imageTitle.add(option);
-                        }
-                    )
+            snapshot.forEach(function (para) {
+                if (para.key == "paragraph") {
+                    para.forEach(function (line) {
+                        addParaDetailsIntoDropDown(line.key)
+                    });
                 }
             });
         });
     }
 }
 
+// function showImage() {
+//     var storage = firebase.storage();
+//     storage.refFromURL(referenceUrl + getSelectedEvent() + "/image/" + getSelectedImage()).getDownloadURL().then(function (url) {
+//         var element = document.createElement("img");
+//         element.setAttribute("src", url);
+//         document.getElementById("showImage").appendChild(element);
+//     });
+// }
+//
+// function deleteImage() {
+//     firebase.storage().refFromURL(referenceUrl + getSelectedEvent() + "/image/" + getSelectedImage()).delete().then(function () {
+//         // File deleted successfully
+//     }).catch(function (error) {
+//         // an error occurred!
+//     });
+//     firebase.database().ref('events/upcoming/' + getSelectedEvent() + "/image/" + getSelectedImage()).remove();
+// }
+
+// function removeOptions(selectbox) {
+//     var imageDiv = document.getElementById("showImage");
+//     while (imageDiv.firstChild) {
+//         imageDiv.removeChild(imageDiv.firstChild);
+//     }
+//     var iterator;
+//     for (iterator = selectbox.options.length - 1; iterator >= 1; iterator--) {
+//         selectbox.remove(iterator);
+//     }
+// }
+
+function checkParagraph() {
+    document.getElementById("paraTitle").value = "";
+    document.getElementById("parawriteup").value = "";
+    var para = document.getElementById("para");
+    var selectedPara = para.options[para.selectedIndex].value;
+    if (selectedPara === "para" || selectedPara === "") {
+        document.getElementById('paratext').style.display = "block"
+    } else {
+        document.getElementById('paratext').style.display = "none";
+        firebase.database().ref('events/upcoming/' + getSelectedEvent() + '/paragraph/' + selectedPara).once('value').then(function (snapshot) {
+            document.getElementById("paraTitle").value = snapshot.val().paratitle;
+            document.getElementById("parawriteup").value = snapshot.val().paradetail;
+        });
+
+    }
+}
+
+function updateEdit() {
+    var selectedEvent = getSelectedEvent();
+    var userDetailText = document.getElementById("parawriteup").value;
+    var userParaTitle = document.getElementById("paraTitle").value;
+    var para = document.getElementById("para");
+    var selectedPara = para.options[para.selectedIndex].value;
+
+    if (selectedEvent == "") {
+        alert("Please select the event");
+    } else if (userDetailText == "") {
+        alert("Please fill the details of paragraph");
+    } else if (userParaTitle == "") {
+        alert("Please fill the paragraph title");
+    } else {
+        if (selectedPara === "para" || selectedPara === "") {
+            var userParaNumber = document.getElementById("paranumber").value;
+            if (userParaNumber == "") {
+                alert("Please enter paragraph number");
+            } else {
+                firebase.database().ref('events/upcoming/' + getSelectedEvent() + '/paragraph/' + userParaNumber).set({
+                    paradetail: userDetailText,
+                    paratitle: userParaTitle
+                });
+            }
+        } else {
+            firebase.database().ref('events/upcoming/' + getSelectedEvent() + '/paragraph/' + selectedPara).update({
+                paradetail: userDetailText,
+                paratitle: userParaTitle
+            });
+        }
+    }
+};
+
 function uploadImage() {
+    var para = document.getElementById("para");
+    var selectedPara = para.options[para.selectedIndex].value;
+
     var userText = document.getElementById("uploadFileName").value;
+    var userDesc = document.getElementById("imagedescription").value;
 
     if (userText == "") {
         alert("Please name the image file");
+    } else if (userDesc == "") {
+        alert("Please name the description");
     }
-    else if (getSelectedEvent() == "") {
-        alert("Please select the event");
+    else if (selectedPara == "" || selectedPara == "para") {
+        alert("Please select the paragraph");
     } else {
-        var storageRef = firebase.storage().ref('events/upcoming/' + getSelectedEvent() + "/image/" + userText);
+        var storageRef = firebase.storage().ref('events/upcoming/' + getSelectedEvent() + '/paragraph/' + selectedPara + "/image/" + userText);
         var $ = jQuery;
         var file = $('#uploadFile').prop('files')[0];
 
@@ -124,41 +206,16 @@ function uploadImage() {
             // console.log('Uploaded a blob or file!');
         });
 
-        firebase.database().ref('events/upcoming/' + getSelectedEvent() + "/image").child(userText).set({
-            date: getSelectedEvent()
+        firebase.database().ref('events/upcoming/' + getSelectedEvent() + '/paragraph/' + selectedPara + "/image").child(userText).set({
+            description: userDesc
         });
 
         document.getElementById("uploadFile").value = "";
         document.getElementById("uploadFileName").value = "";
+        document.getElementById("imagedescription").value = "";
     }
 }
 
-function showImage() {
-    var storage = firebase.storage();
-    storage.refFromURL(referenceUrl + getSelectedEvent() + "/image/" + getSelectedImage()).getDownloadURL().then(function (url) {
-        var element = document.createElement("img");
-        element.setAttribute("src", url);
-        document.getElementById("showImage").appendChild(element);
-    });
-}
 
-function deleteImage() {
-    firebase.storage().refFromURL(referenceUrl + getSelectedEvent() + "/image/" + getSelectedImage()).delete().then(function () {
-        // File deleted successfully
-    }).catch(function (error) {
-        // an error occurred!
-    });
-    firebase.database().ref('events/upcoming/' + getSelectedEvent() + "/image/" + getSelectedImage()).remove();
-}
 
-function removeOptions(selectbox) {
-    var imageDiv = document.getElementById("showImage");
-    while (imageDiv.firstChild) {
-        imageDiv.removeChild(imageDiv.firstChild);
-    }
-    var iterator;
-    for (iterator = selectbox.options.length - 1; iterator >= 1; iterator--) {
-        selectbox.remove(iterator);
-    }
-}
 
--- js/spiritual.js
+++ js/spiritual.js
@@ -7,16 +7,26 @@ function readData() {
                 year.forEach(function (month) {
                     month.forEach(function (day) {
                         if (day.val().type == "spiritual") {
-                            var imageArr = [];
-                            day.forEach(function (image) {
-                                if (image.key == "date" || image.key == "message" || image.key == "type" || image.key == "detail") {
-                                } else {
-                                    image.forEach(function (names) {
-                                        imageArr.push(year.key + "/" + month.key + "/" + day.key + "/image/" + names.key)
+                            addDetailsToUI(day.val().date, day.val().message);
+                            day.forEach(function (para) {
+                                if (para.key == "paragraph") {
+                                    para.forEach(function (line) {
+                                        var imageArr = [];
+                                        var description = [];
+                                        line.forEach(function (image) {
+                                            if (image.key == "image") {
+                                                image.forEach(function (names) {
+                                                    imageArr.push(year.key + "/" + month.key + "/" + day.key + "/" + para.key + "/" + line.key + "/" + image.key + "/" + names.key);
+                                                    names.forEach(function (desc) {
+                                                        description.push(names.val().description)
+                                                    })
+                                                });
+                                            }
+                                        });
+                                        addContentIntoUI(day.val().date, line.key, line.val().paratitle, line.val().paradetail, imageArr, description)
                                     });
                                 }
                             });
-                            addDetailsToUI(day.val().date, day.val().message, day.val().detail, imageArr);
                         }
                     })
                 });
@@ -40,22 +50,34 @@ function readData() {
 
 }
 
-function addDetailsToUI(date, message, detail, imageArr) {
+function addContentIntoUI(date, idkey, title, detail, imageArr, imagedescription) {
     var modifiedDate = date.replace(/ /g, "_");
-    var initial = "<article> <div class=\"heading\"> <h2><a>" + message + "</a></h2> <p class=\"info\">>>>" + date + "</p> </div> <div class=\"content\" id=\"" + modifiedDate + "\"> <p>" + detail + "</p>"
-    var image = "";
-
+    var modifiedTitle = idkey;
+    var initial = "<article> <div class=\"heading\"> <p class=\"info\"><h4><i>" + title + "</i></h4></p> </div> <div class=\"content\" id=\"" + modifiedTitle + "\"></div><div> <p align=\"justify\">" + detail + "</p></div> </article>";
     for (var imageCount = 0; imageCount < imageArr.length; imageCount++) {
         var storage = firebase.storage();
         storage.refFromURL(referenceUrl + imageArr[imageCount]).getDownloadURL().then(function (url) {
-            var element = document.createElement("img");
-            element.setAttribute("src", url);
-            document.getElementById(modifiedDate).appendChild(element);
+            var figure = document.createElement("figure");
+            var figcaption = document.createElement("figcaption");
+            var description = document.createTextNode(imagedescription[imageCount - 1]);
+            figcaption.appendChild(description);
+            var image = document.createElement("img");
+            image.setAttribute("src", url);
+            figure.appendChild(image);
+            figure.appendChild(figcaption);
+            document.getElementById(modifiedTitle).prepend(figure);
         }).catch(function (error) {
         });
     }
+    var element = initial;
+    $("#" + modifiedDate).append(element);
+}
+
+function addDetailsToUI(date, message) {
+    var modifiedDate = date.replace(/ /g, "_");
+    var initial = "<article> <div class=\"heading\"> <h2><a>" + message + "</a></h2> <p class=\"info\">>>>" + date + "</p> </div> <div class=\"content\"> <div id=\"" + modifiedDate + "\"></div></div>";
     var final = "</div> </article>";
-    var element = initial + image + final;
+    var element = initial + final;
     $(".main-content").prepend(element);
 }
  
