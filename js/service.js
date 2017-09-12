310df99bcec009706c8a1d8c61c6fec18a39533b
abilashr <abilaashsai@gmail.com>
2017-09-12 20:21:17 +0530

    add collapsing functionality to service wing

--- js/service.js
+++ js/service.js
@@ -7,16 +7,26 @@ function readData() {
                 year.forEach(function (month) {
                     month.forEach(function (day) {
                         if (day.val().type == "service") {
-                            var imageArr = [];
-                            day.forEach(function (image) {
-                                if (image.key == "date" || image.key == "message" || image.key == "type" || image.key == "detail") {
-                                } else {
-                                    image.forEach(function (names) {
-                                        imageArr.push(year.key + "/" + month.key + "/" + day.key + "/image/" + names.key)
+                            addDetailsToUI(day.val().date, day.val().message);
+                            day.forEach(function (para) {
+                                if (para.key == "paragraph") {
+                                    para.forEach(function (line) {
+                                        var imageArr = [];
+                                        var description = [];
+                                        line.forEach(function (image) {
+                                            if (image.key == "image") {
+                                                image.forEach(function (names) {
+                                                    imageArr.push(year.key + "/" + month.key + "/" + day.key + "/" + para.key + "/" + line.key + "/" + image.key + "/" + names.key);
+                                                    names.forEach(function (desc) {
+                                                        description.push(names.val().description)
+                                                    })
+                                                });
+                                            }
+                                        });
+                                        addContentIntoUI(day.val().date, line.key, line.val().paradetail, imageArr, description)
                                     });
                                 }
                             });
-                            addDetailsToUI(day.val().date, day.val().message, day.val().detail, imageArr);
                         }
                     })
                 });
@@ -25,36 +35,63 @@ function readData() {
         function scrollToSpecificLocation() {
             var url_string = window.location.href;
             var url = new URL(url_string);
-            var scrollId = "#" + url.searchParams.get("date");
+            var scrollId = "#" + url.searchParams.get("date") + "heading";
             if (url.searchParams.get("date") != null) {
                 $(document).ready(function () {
                     $('html, body').animate({
                         scrollTop: $(scrollId).offset().top
                     }, 'slow');
+                    $(scrollId).trigger('click');
                 });
             }
         }
 
         scrollToSpecificLocation();
     });
+
 }
 
-function addDetailsToUI(date, message, detail, imageArr) {
+function addContentIntoUI(date, idkey, detail, imageArr, imagedescription) {
     var modifiedDate = date.replace(/ /g, "_");
-    var initial = "<article> <div class=\"heading\"> <h2><a>" + message + "</a></h2> <p class=\"info\">>>>" + date + "</p> </div> <div class=\"content\" id=\"" + modifiedDate + "\"> <p>" + detail + "</p>";
-    var image = "";
-
+    var modifiedTitle = modifiedDate + idkey;
+    var element = "<article> <div class=\"content\" id=\"" + modifiedTitle + "\"></div><div> <p align=\"justify\">" + detail + "</p></div> </article>";
     for (var imageCount = 0; imageCount < imageArr.length; imageCount++) {
         var storage = firebase.storage();
         storage.refFromURL(referenceUrl + imageArr[imageCount]).getDownloadURL().then(function (url) {
-            var element = document.createElement("img");
-            element.setAttribute("src", url);
-            document.getElementById(modifiedDate).appendChild(element);
+            var figure = document.createElement("figure");
+            var figcaption = document.createElement("figcaption");
+            var description = document.createTextNode(imagedescription[imageCount - 1]);
+            figcaption.appendChild(description);
+            var image = document.createElement("img");
+            image.setAttribute("src", url);
+            figure.appendChild(image);
+            figure.appendChild(figcaption);
+            document.getElementById(modifiedTitle).prepend(figure);
         }).catch(function (error) {
         });
     }
-    var final = "</div> </article>";
-    var element = initial + image + final;
+    $("#" + modifiedDate).append(element);
+}
+
+function addDetailsToUI(date, message) {
+    var modifiedDate = date.replace(/ /g, "_");
+    var headingId = modifiedDate + "heading";
+    var contentId = modifiedDate + "content";
+    var contentIdentifier = "#" + modifiedDate + "content";
+    var element = `<article>
+                                <div class=\"panel panel-default\">
+                                    <div class=\"heading panel-heading\" data-toggle=\"collapse\" id = ${headingId} href=${contentIdentifier}>
+                                        <h2><a>${message}</a></h2>
+                                            <p class=\"info\">>>>${date}</p>
+                                    </div>
+                                    <div class=\"content panel-collapse collapse\" id = ${contentId} >
+                                        <div id=${modifiedDate}>
+                                        </div>
+                                    </div>
+                                </div>
+                     </article>
+                  `;
+
     $(".main-content").prepend(element);
 }
  
